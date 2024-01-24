60d657a4568fa653f8507fd88dc5ee4beb3ec677
Alejandro Hdez <alherdom@outlook.com>
2024-01-24 20:28:51 +0000

    Created: utils to handle csv files.

--- loaders/utils.py
+++ loaders/utils.py
@@ -1,30 +1,31 @@
-import csv
+from csv import DictReader
 from matches.models import Match
 from players.models import Player
 from datetime import datetime
 
 
-def handle_uploaded_file(matches, players, stats):
+def handle_uploaded_players(players):
     decoded_players = players.read().decode("utf-8")
-    decoded_matches = matches.read().decode("utf-8")
-    decoded_stats = stats.read().decode("utf-8")
-    reader_players = csv.DictReader(decoded_players.splitlines())
-    reader_matches = csv.DictReader(decoded_matches.splitlines())
-    reader_stats = csv.DictReader(decoded_stats.splitlines())
+    reader_players = DictReader(decoded_players.splitlines())
 
     for row in reader_players:
         if row["birthdate"] == "":
             row["birthdate"] = "1900-01-01"
             Player.objects.create(
-            player_atp=row["player_id"],
-            name=row["name"],
-            hand=row["hand"],
-            country=row["country"],
-            birthdate=datetime.strptime(row["birthdate"], "%Y-%m-%d"),
-        )
+                player_atp=row["player_id"],
+                name=row["name"],
+                hand=row["hand"],
+                country=row["country"],
+                birthdate=datetime.strptime(row["birthdate"], "%Y-%m-%d"),
+            )
+
+
+def handle_uploaded_matches(matches):
+    decoded_matches = matches.read().decode("utf-8")
+    reader_matches = DictReader(decoded_matches.splitlines())
 
     for row in reader_matches:
-            Match.objects.create(
+        Match.objects.create(
             match_atp=row["match_id"],
             tournament=row["tournament"],
             date=datetime.strptime(row["date"], "%Y-%m-%d"),
@@ -32,13 +33,15 @@ def handle_uploaded_file(matches, players, stats):
             duration=row["duration"],
         )
 
+
+def handle_uploaded_stats(stats):
+    decoded_stats = stats.read().decode("utf-8")
+    reader_stats = DictReader(decoded_stats.splitlines())
+
     for row in reader_stats:
         player = row["player_id"]
         match = Match.objects.get(match_atp=row["match_id"])
         if row["winner"] == "TRUE":
-            winner = Player.objects.get(player_atp=player)
-            match.winner = winner
-        else:
-            loser = Player.objects.get(player_atp=player)
-            match.loser = loser
+            match.winner = Player.objects.get(player_atp=player)
+        match.loser = Player.objects.get(player_atp=player)
         match.save()
--- loaders/views.py
+++ loaders/views.py
@@ -1,5 +1,9 @@
 from .forms import UploadFilesForm
-from .utils import handle_uploaded_file
+from .utils import (
+    handle_uploaded_players,
+    handle_uploaded_matches,
+    handle_uploaded_stats,
+)
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
@@ -14,10 +18,9 @@ def upload_files(request):
     if request.method == "POST":
         form = UploadFilesForm(request.POST, request.FILES)
         if form.is_valid():
-            matches = request.FILES["matches"]
-            players = request.FILES["players"]
-            stats = request.FILES["stats"]
-            handle_uploaded_file(matches, players, stats)
+            handle_uploaded_players(request.FILES["players"])
+            handle_uploaded_matches(request.FILES["matches"])
+            handle_uploaded_stats(request.FILES["stats"])
             return redirect("loaders:upload_ok")
     else:
         form = UploadFilesForm()
